b888adef79aa10845f4f73d785260e91de5b291a
Triantafyllou <niki.triantafyllou20@imperial.ac.uk>
2025-04-09 19:46:10 +0100

    Stop tracking virtual environment

---
+++ GSA/DME/Results/Metamodels/Parabola.d
@@ -0,0 +1,27 @@
+Input file for model Parabola.
+Warning, dont use in the comments character 'colon'.
+
+Var Number :	17
+
+0=< Lin Constr ( matrix A): 0
+
+0=<  NonLin Constr  (C(X) dimension):	0
+
+BL for Var:
+	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+
+BU for Var:
+	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1
+
+Matrix A: None
+
+BL for A*X: None
+
+BU for A*X: None
+
+BL for C(X): None
+
+BU for C(X): None
+
+Number of integer variables : 0
+
